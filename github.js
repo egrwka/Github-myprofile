36f86cd1c3a6abdce4beaf4bf9a0de47ec516c2b
Opilane TTHK <opilane@tthk.ee>
2019-12-09 14:47:15 +0200

    last my project

---
+++ github.js
@@ -0,0 +1,28 @@
+class GitHub{
+    constructor(){
+        this.client_id='33bc87a4f554d8b0f6d9';
+        this.client_secret='984a8ee7e85621e5ecff85f0658faa54b862180c';
+        this.repos_count=5;
+        this.repos_sort='created: asc';
+        this.user='egrwka';
+    }
+
+    //get a user info
+
+    async getUserData(){
+        //`` - backticks
+        let urlProfile= `https://api.github.com/users/${this.user}?client_id=${this.client_id}&client_secret=${this.client_secret}`;
+        let urlRepos= `https://api.github.com/users/${this.user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`;
+        
+        const profileResponse = await fetch(urlProfile);
+        const reposResponse = await fetch(urlRepos)
+
+        const profile = await profileResponse.json();
+        const repos =  await reposResponse.json();
+
+        return{
+            profile,
+            repos
+        }
+    }
+}
